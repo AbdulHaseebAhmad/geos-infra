cf236a5573919146afc56bcb3ec20c4190fe73b6
Abdul Haseeb Ahmad Bashir <abdull.haseebkhan@gmail.com>
2026-08-13 10:18:35 +0100

    update useer-data script to backend service commands

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -3,7 +3,30 @@
 
 apt-get update -y
 apt-get install -y nginx
-mkdir -p /var/www/geos-backend
-mkdir -p /var/www/geos-frontend
+sudo mkdir -p /var/www/geos-backend
+sudo mkdir -p /var/www/geos-frontend
+
+sudo chown ubuntu:ubuntu /var/www/geos-backend
+sudo chown ubuntu:ubuntu /var/www/geos-frontend
+
+cat <<EOF > /etc/systemd/system/geos-backend.service
+[Unit]
+Description=GEOS Backend Service
+After=network.target
+
+[Service]
+ExecStart=/var/www/geos-backend/geos-backend
+Restart=always
+User=ubuntu
+WorkingDirectory=/var/www/geos-backend
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+systemctl daemon-reload
+systemctl enable geos-backend
+
 systemctl enable nginx
 systemctl start nginx
